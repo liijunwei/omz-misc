c7f41fb15a8adcb0be68b766fbd2d3e3e1b7a3ef
lijunwei <ljw532344863@sina.com>
2022-07-18 17:59:10 +0800

    Cleanup

--- gem_search.sh
+++ gem_search.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-alias gem-search-local=" gem list -l | awk '{print \$1}' | fzf | xargs -I {} open \"https://rubygems.org/search?query={}\""
-alias gem-search-remote="gem list -r | awk '{print \$1}' | fzf | xargs -I {} open \"https://rubygems.org/search?query={}\""
-
+function gem-search-local() {
+  gem list -l |
+  awk '{print $1}' |
+  fzf |
+  xargs -I {} open "https://rubygems.org/search?query={}"
+}
