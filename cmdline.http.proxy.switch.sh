336b5f00f419b339238fe381e173cba90c19dd27
lijunwei <ljw532344863@sina.com>
2022-09-20 22:13:34 +0800

    Update proxy command

--- cmdline.http.proxy.switch.sh
+++ cmdline.http.proxy.switch.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 function proxy_on(){
-    export http_proxy=http://127.0.0.1:1087
-    export https_proxy=http://127.0.0.1:1087
-    echo -e "命令行代理已开启..."
+    export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+    echo -e "proxy enabled..."
 }
 
 function proxy_off(){
     unset http_proxy
     unset https_proxy
-    echo -e "命令行代理已关闭..."
+    echo -e "proxy disabled..."
 }
