4dc45c5d82235ab28c80617e3e179bc327237321
junwei <ljw532344863@sina.com>
2023-08-12 10:37:35 +0800

    Optimize gem-open-code

--- gem_browse_code.sh
+++ gem_browse_code.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 function gem-open-code() {
-  local gemname=$(gem list -l | fzf | awk '{print $1}')
+  local keyword=$1
+
+  if [[ "" == "$keyword" ]]; then
+    local gemname=$(gem list -l | fzf | awk '{print $1}')
+  else
+    local gemname=$(gem list -l | fzf -q $keyword | awk '{print $1}')
+  fi
+
   $CORE_EDITOR $(bundle info $gemname --path)
 }
 
