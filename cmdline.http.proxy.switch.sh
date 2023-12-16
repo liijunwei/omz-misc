6552ccff65f8df728d994e4e4421ac4e861ce19b
junwei <ljw532344863@sina.com>
2023-12-16 10:31:59 +0800

    Fix unset not working issue

--- cmdline.http.proxy.switch.sh
+++ cmdline.http.proxy.switch.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 function proxy_on(){
-    export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+    export http_proxy=http://127.0.0.1:7890
+    export https_proxy=http://127.0.0.1:7890
+    export all_proxy=socks5://127.0.0.1:7890
     echo -e "proxy enabled..."
 }
 
 function proxy_off(){
     unset http_proxy
     unset https_proxy
+    unset all_proxy
     echo -e "proxy disabled..."
 }
