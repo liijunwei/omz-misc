9eadd7408e943154734a299728c86995b9185b2c
junwei <ljw532344863@sina.com>
2024-11-13 14:41:21 +0800

    Commit manually

--- find-ruby-warnings.sh
+++ find-ruby-warnings.sh
@@ -3,5 +3,8 @@
 function find-ruby-warnings() {
   local directory=${1:-.}
 
+  echo "it's not returning msg, it's print to std"
+  echo "require 'parallel'; Parallel.each(Dir.glob(\"**/*.rb\"), progress: 'checking') {|file| \`ruby -wc #{file}\`}"
+
   parallel "ruby -wc {}" ::: $(find $directory -name \*.rb) 1> /dev/null
 }
